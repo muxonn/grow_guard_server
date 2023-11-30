ab057b08a95b10d81eae9f661a444f74eafad9ba
Jakub Madry <jaiwiecejmnie@gmail.com>
2023-12-01 00:27:22 +0100

    Add installed aps and create sensor model

---
+++ grow_guard/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Sensor(models.Model):
+    name = models.CharField(max_length = 50)
+    value = models.FloatField(max_length = 50)
+
+    def __str__(self):
+        return self.name
