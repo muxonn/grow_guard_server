c956e2eaa908a32570f79836d30c708b330dd36d
Jakub Madry <jaiwiecejmnie@gmail.com>
2023-12-16 01:44:00 +0100

    Add 3 sensors models and serializers

--- grow_guard/models.py
+++ grow_guard/models.py
@@ -7,6 +7,30 @@ class Device(models.Model):
     def __str__(self):
         return self.name
 
+class Temperature(models.Model):
+    device = models.ForeignKey(Device, related_name = 'temperatures', on_delete = models.CASCADE)
+    value = models.FloatField(max_length = 50)
+    timestamp = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.value
+
+class Humidity(models.Model):
+    device = models.ForeignKey(Device, related_name = 'humidities', on_delete = models.CASCADE)
+    value = models.FloatField(max_length = 50)
+    timestamp = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.value
+
+class Lighting(models.Model):
+    device = models.ForeignKey(Device, related_name = 'lighting', on_delete = models.CASCADE)
+    value = models.FloatField(max_length = 50)
+    timestamp = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.value
+
 class Sensor(models.Model):
     device = models.ForeignKey(Device, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length = 50)
--- grow_guard/serializers.py
+++ grow_guard/serializers.py
@@ -1,15 +1,31 @@
 from rest_framework import serializers
-from .models import Sensor, Device, Camera
-
-class SensorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Sensor
-        fields = ['name', 'value']
+from .models import Sensor, Device, Camera, Temperature, Humidity, Lighting
 
 class DeviceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Device
         fields = ['id', 'name']
+
+class TemperatureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Temperature
+        fields = ['device', 'value', 'timestamp']
+
+class LightingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lighting
+        fields = ['device', 'value', 'timestamp']
+
+class HumiditySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Humidity
+        fields = ['device', 'value', 'timestamp']
+
+
+class SensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ['name', 'value']
 
 class CameraSerializer(serializers.ModelSerializer):
     class Meta:
